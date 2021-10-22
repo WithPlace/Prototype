e7c002ded37e5140bbb6a188a54bdcf2b2faafb3
LeeYeJi <lwj1748@naver.com>
2021-10-23 00:28:00 +0900

    Edit config files

--- src/main/java/com/example/withplaceprototype/WithPlacePrototypeApplication.java
+++ src/main/java/com/example/withplaceprototype/WithPlacePrototypeApplication.java
@@ -1,13 +1,16 @@
 package com.example.withplaceprototype;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class WithPlacePrototypeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WithPlacePrototypeApplication.class, args);
+        log.info("Hi");
     }
 
 }
