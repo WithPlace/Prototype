ccfb3ec555a49af9179e6964d76f539045fd83ec
LeeYeJi <lwj1748@naver.com>
2021-10-23 00:21:24 +0900

    initial commit

---
+++ src/main/java/com/example/withplaceprototype/WithPlacePrototypeApplication.java
@@ -0,0 +1,13 @@
+package com.example.withplaceprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithPlacePrototypeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WithPlacePrototypeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/withplaceprototype/WithPlacePrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.withplaceprototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithPlacePrototypeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
